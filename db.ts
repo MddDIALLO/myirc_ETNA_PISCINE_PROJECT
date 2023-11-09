d503496f4b04b12ac8777907f7945e664e1a472c
MddDiallo <122102997+MddDIALLO@users.noreply.github.com>
2023-11-09 15:52:59 +0100

    Update

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
     host: 'localhost',
-    user: 'mysqldbuser',
-    password: 'ConnectMe123$',
+    user: 'zertyu',
+    password: 'sdfyu',
     database: 'myirc_db'
 });
